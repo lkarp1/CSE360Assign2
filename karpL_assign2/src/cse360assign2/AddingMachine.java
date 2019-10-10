840a0ca0b99c3133bda895267d88cebd62c766f8
Laura Karp <lkarp1@asu.edu>
2019-10-09 21:23:50 -0700

    Initial version

---
+++ karpL_assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,66 @@
+/**
+ * @author Laura Karp
+ * Class ID: 70642
+ * Description: program that includes several functions simulating adding and substracting 
+ * @version 3.0
+ * @date 10/9/19
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	
+	/**
+	 * Global variable
+	 */
+	private int total;
+	
+	
+	/**
+     * A constructor for the AddingMachine class
+     * @param no parameter necessary
+     */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+     * method to determine the total
+     * @return 0
+     */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+     * method that adds values
+     * @param value value is an int that will be added
+     */
+	public void add (int value) {
+		
+	}
+	
+	/**
+     * method that subtracts values
+     * @param value value is an int that will be subtracted
+     */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+     * method that changes input to a string type
+     * @return "" "" is an empty string
+     */
+	public String toString () {
+		return "";
+	}
+
+	/**
+     * method that clears input, simulates a clear button on a calculator
+     */
+	public void clear() {
+	
+	}
+}
